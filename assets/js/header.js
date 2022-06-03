e91b7564a61c1c6299b0ce7dd6983c6d9b30f844
jv-miranda <joao.vitor.miranda.siqueira@gmail.com>
2022-06-03 19:48:42 -0300

    Menu Added

---
+++ assets/js/header.js
@@ -0,0 +1,29 @@
+let logo = document.querySelector(`.logo`)
+
+function headerOnScroll() {
+  let header = document.querySelector(`header`)
+  let header_button_open = document.querySelector(`.header_button_open_svg`)
+
+  if (scrollY == 0) {
+    header.classList.remove(`onScroll`)
+    logo.setAttribute(`src`, `assets/svgs/logo.svg`)
+    header_button_open.setAttribute(`src`, `assets/svgs/header_button_open.svg`)
+  } else {
+    header.classList.add(`onScroll`)
+    logo.setAttribute(`src`, `assets/svgs/logo_onscroll.svg`)
+    header_button_open.setAttribute(
+      `src`,
+      `assets/svgs/header_button_onscroll_open.svg`
+    )
+  }
+}
+
+function openMenu() {
+  document.body.classList.add(`menu_expanded`)
+  logo.setAttribute(`src`, `assets/svgs/logo_onscroll.svg`)
+}
+
+function closeMenu() {
+  document.body.classList.remove(`menu_expanded`)
+  logo.setAttribute(`src`, `assets/svgs/logo.svg`)
+}
